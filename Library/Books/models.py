beddf4e108adb06e27e1ac72fd96619a0167619a
Merari Cisneros <meraridanielcisneros@gmail.com>
2023-07-09 22:39:10 -0700

    Created Book Model and ran migration for the Book App

--- Library/Books/models.py
+++ Library/Books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    Name = models.CharField(max_length=25)
+    ISBN = models.IntegerField( primary_key=True)
+    pub_date = models.DateField("Date Published")
+    Image = models.ImageField("Book Cover")
+    Summary = models.CharField(max_length= 500)
